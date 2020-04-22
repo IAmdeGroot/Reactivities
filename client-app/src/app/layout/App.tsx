dd0de074615d40f23011f4985de3b7435d8e8ea0
Johan de Groot <johan.groot@webking.se>
2020-04-22 15:35:35 +0200

    end of section 5

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from "react";
+import { Container } from "semantic-ui-react";
+import axios from "axios";
+import { IActivity } from "../models/activity";
+import { NavBar } from "../../features/nav/NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(
+    null
+  );
+
+  const [editMode, setEditMode] = useState(false);
+
+  const handleSelectActivity = (id: string) => {
+    setSelectedActivity(activities.filter((a) => a.id === id)[0]);
+    setEditMode(false);
+  };
+
+  const handleOpenCreateForm = () => {
+    setSelectedActivity(null);
+    setEditMode(true);
+  };
+
+  const handleCreateActivity = (activity: IActivity) => {
+    setActivities([...activities, activity])
+    //This spreads the existing array of activities and inserts a new activity in it.
+
+    setSelectedActivity(activity);
+    setEditMode(false);
+  }
+
+  const handleEditActivity = (activity: IActivity) => {
+    setActivities([...activities.filter(a => a.id !== activity.id), activity])
+    //This spreads the existing array of activities containing all activities except the one we are currently editing.
+    //We insert the edited activity inside the new array.
+
+    setSelectedActivity(activity);
+    setEditMode(false);
+  }
+
+  const handleDeleteActivity = (id: string) => {
+    setActivities([...activities.filter(a => a.id !==id )])
+  }
+
+  //Combination of componentDidMount, componentDidUpdate, componentDidUmount.
+  //Gets called on every render - if not ending with "[]"
+  useEffect(() => {
+    axios
+      .get<IActivity[]>("http://localhost:5000/api/activities")
+      .then((response) => {
+        let activities: IActivity[] = [];
+        response.data.forEach((activity) => {
+          activity.date = activity.date.split('.')[0];
+          activities.push(activity);
+        });
+        setActivities(activities);
+      });
+  }, []);
+
+  // [] means that the useEffect will run only once, like componentDidMount.
+  return (
+    <>
+      <NavBar openCreateForm={handleOpenCreateForm}></NavBar>
+      <Container style={{ marginTop: "7em" }}>
+        <ActivityDashboard
+          activities={activities}
+          selectActivity={handleSelectActivity}
+          selectedActivity={selectedActivity}
+          editMode={editMode}
+          setEditMode={setEditMode}
+          setSelectedActivity={setSelectedActivity}
+          createActivity={handleCreateActivity}
+          editActivity={handleEditActivity}
+          deleteActivity={handleDeleteActivity}
+        />
+      </Container>
+    </>
+  );
+};
+
+export default App;
